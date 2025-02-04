d359714f4b908be7e7b246cad2b7875af9033998
qwerdfJIN <test19980224@gmail.com>
2025-02-04 11:30:19 +0900

    README 수정

--- schedule.sql
+++ schedule.sql
@@ -4,7 +4,7 @@ USE schedule;
 CREATE TABLE schedule (
                           id INT AUTO_INCREMENT PRIMARY KEY,
                           writer VARCHAR(10) NOT NULL,
-                          password VARCHAR(8) NOT NULL,
+                          password CHAR(8) NOT NULL,
                           content TEXT NOT NULL,
                           created_date DATETIME DEFAULT CURRENT_TIMESTAMP,
                           updated_date DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
